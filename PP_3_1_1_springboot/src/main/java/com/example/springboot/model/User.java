852294f42479a72aeb190124673b9f6e328c14e4
eduardaccount <100372621+eduardaccount@users.noreply.github.com>
2022-03-23 21:25:12 +0500

    Update User.java

--- PP_3_1_1_springboot/src/main/java/com/example/springboot/model/User.java
+++ PP_3_1_1_springboot/src/main/java/com/example/springboot/model/User.java
@@ -1,13 +1,10 @@
 package com.example.springboot.model;
 
-import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users_table")
-@Data
-@NoArgsConstructor
 public class User {
 
     @Id
@@ -21,10 +18,62 @@ public class User {
     @Column(name = "user_age")
     private byte userAge;
 
+    public User() {
+    }
+
+    public User(long userId, String userName, String userEmail, byte userAge) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userAge = userAge;
+    }
+
+
     public User(String userName, String userEmail, byte userAge) {
         this.userId = userId;
         this.userName = userName;
         this.userEmail = userEmail;
         this.userAge = userAge;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public byte getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(byte userAge) {
+        this.userAge = userAge;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userAge=" + userAge;
     }
 }
